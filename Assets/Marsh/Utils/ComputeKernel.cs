012c81ec41ef7acb36f46b12e8817dc04655e78f
Jayesskay <jayeskdev@gmail.com>
2022-05-23 18:17:03 +0200

    Added ComputeKernel utility

---
+++ Assets/Marsh/Utils/ComputeKernel.cs
@@ -0,0 +1,32 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ComputeKernel
+{
+    private ComputeShader _shader;
+    private int _index;
+    private int3 _threadGroupSize;
+
+    public ComputeKernel(ComputeShader shader, string name)
+    {
+        _shader = shader;
+        _index = shader.FindKernel(name);
+        shader.GetKernelThreadGroupSizes(_index, out uint x, out uint y, out uint z);
+        _threadGroupSize = new((int)x, (int)y, (int)z);
+    }
+
+    public void DispatchDivByThreadGroupSize(int x, int y, int z)
+    {
+        _shader.Dispatch(_index, x / _threadGroupSize.x, y / _threadGroupSize.y, z / _threadGroupSize.z);
+    }
+
+    public void SetBuffer(string name, ComputeBuffer buffer)
+    {
+        _shader.SetBuffer(_index, name, buffer);
+    }
+
+    public void SetFloat3(string name, float3 v)
+    {
+        _shader.SetFloats(name, v.x, v.y, v.z);
+    }
+}
